cfc1a637bf449f163b57b6d5be1010479dabbdd5
John McLear <john@mclear.co.uk>
2012-01-29 21:06:18 +0000

    add example config that is tracked

---
+++ example.config.php
@@ -0,0 +1,17 @@
+<?php
+/*  Config -- EDIT THESE SETTINGS!
+    ------*/
+
+$test_only = true; // should we only do a test run and not actually copy/paste anything?  A test run will output the stages but wont download or create any files on vimeo.
+$youtube_username = 'meh'; // your youtube username
+$vimeo_consumer_key = ''; // your vimeo consumer key
+$vimeo_secret = ''; // your secret from vimeo
+$logpath = "/var/log/youtube_to_vimeo.txt";
+
+// DO NOT EDIT BELOW THIS LINE
+$settings["test_only"] = $test_only;
+$settings["youtube_username"] = $youtube_username;
+$settings["vimeo_consumer_key"] = $vimeo_consumer_key;
+$settings["vimeo_secret"] = $vimeo_secret;
+$settings["logpath"] = $logpath;
+?>
